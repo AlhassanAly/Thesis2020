6d7a5c3cd73563e97523214866b95ea94c111ef5
unknown <alhassan.ali@est.fib.upc.edu>
2020-04-04 02:20:40 +0200

    clustering

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import pylab
 import math
 import json   
 import random 
-
+import itertools
 
 G = nx.Graph(name = "Cloud-Fog-Network")
 G.add_node("C")
 
 medium_speed = 299792458  # speed of light
-fogs = 5
+fogs = 10
 fog_range = 0.4
 Edge_devices = 5 
 
+positions ={}
+attributes = {}
+fog_list = []
 
 for n in range (1,fogs+1):
     G.add_node("f" + str(n))
@@ -28,9 +31,6 @@ node_attrs = {"C":{"MIPS": 5000, "STR": 500000}}
 
 def place_fogs(x1 = -0.7,x2 = 0.7 ,y1 = -0.3, y2 = 0.1, mips_low = 200, mips_high=900, 
 str_low = 512, str_high = 2048):
-    positions ={}
-    attributes = {}
-    fog_list = []
     for number in range (1,fogs+1):
         positions = {"f" + str(number):(random.uniform(x1,x2),random.uniform(y1, y2))}
         attributes = {"f" + str(number):{"MIPS":random.randint(mips_low,mips_high),"STR":random.randint(str_low, str_high)}}
@@ -58,6 +58,63 @@ def place_devices(x1=-0.8, x2=0.8, y1=-0.6, y2=0.1, ins_low= 50, ins_high=2000 ,
         node_attrs.update(attributes)
     return node_positions, device_list, attributes
 
+def inRange_Clustering(candidate_list):
+
+    cluster_list ={}  
+    min_cluster = {}
+
+    #new approach
+    for fold in range(1, len(candidate_list)+1):
+        for comb in itertools.combinations(candidate_list, fold):
+            storage = 0
+            for node in comb:
+                storage += G.nodes[node]["STR"]
+                if storage >= G.nodes[d]["Size"]:
+                    cluster_list[comb] = storage
+    if cluster_list != {}:
+        minimum_comb = min(cluster_list, key = cluster_list.get)
+        minimum_ST = cluster_list[minimum_comb]
+        min_cluster[minimum_comb]=minimum_ST           
+    return cluster_list, min_cluster
+
+
+def getMaxStorage(candidate_list):
+    max_storage = 0
+    for node in candidate_list:
+        max_storage += G.nodes[node]["STR"]   
+    return max_storage
+
+def fogNeighbour_Clustering(d, candidate_list):
+
+    neighbour_list = []
+    all_neighbours = {}
+    neighbor_candidates = []
+    max_storage = getMaxStorage(candidate_list)
+
+    for fog in candidate_list:
+        for global_fog in fog_list:
+            if fog != global_fog:
+                fog_position = fnode_pos[fog]
+                global_fog_position = fnode_pos[global_fog]
+                dist = calculateDistance(fog_position[0], fog_position[1], global_fog_position[0], global_fog_position[1])
+                if dist < fog_range:
+                    neighbour_list.append(global_fog)
+                      
+        all_neighbours[fog] = neighbour_list
+    
+    print ("all neighbours", all_neighbours)
+
+    for fog, neighbour in all_neighbours.items():
+       
+        neighbor_candidates.append(fog)
+        for n in neighbour:
+            if (max_storage + G.nodes[n]["STR"]) >=  G.nodes[d]["Size"]:
+                G.add_edges_from([(fog, n)])
+                neighbor_candidates.append(n)
+        
+    return neighbor_candidates
+
+       
 node_pos, device_list, attributes = place_devices()
 
 nx.set_node_attributes(G, node_attrs)
@@ -109,19 +166,21 @@ def  add_link_attributes(data_rate_low = 1000, data_rate_high = 10000):
     return link_attrs
 
 add_link_attributes()
-nx.set_edge_attributes(G, link_attrs) 
-
+nx.set_edge_attributes(G, link_attrs)
 
 candidate_list = []
-cluster_list = []
+
 for d in G.nodes:
     if 'd' in d:
         candidate_list = list(G.adj[d])
-        print("candidate_list for", d, ":", candidate_list)
-        if candidate_list != None:
+        if candidate_list != []:
+            print("candidate list for", d, ":", candidate_list)
             response_times = {}
+            found_fog = False
             for candidate in candidate_list:
+
                 if G.nodes[candidate]["STR"] >= G.nodes[d]["Size"]:
+                    found_fog = True
                     print(candidate +" STR:", G.nodes[candidate]["STR"], d + " Size:", G.nodes[d]["Size"], " Valid capacity")
                     link_attributes = G.edges[str(candidate), str(d)]
                     DR = link_attributes['DR']
@@ -135,59 +194,34 @@ for d in G.nodes:
                     r_t = processing_time + network_latency
                     response_times[candidate] = r_t
                     print("response times for",d, ":", response_times) 
-                elif G.nodes[candidate]["STR"] < G.nodes[d]["Size"]:
-                    print(candidate +" STR:", G.nodes[candidate]["STR"], d + " Size:", G.nodes[d]["Size"], " Invalid capacity")
-                    for candidate2 in candidate_list:
-                        if G.nodes[candidate]["STR"] + G.nodes[candidate2]["STR"]  >= G.nodes[d]["Size"]:
-                            if candidate != candidate2 and (candidate2, candidate) not in cluster_list:
-                                cluster_list = ((candidate, candidate2))
-            if cluster_list != []:
-                print("cluster list for", d, ":" , cluster_list) 
-                    
+            
+            if not found_fog:
+
+                    cluster_list, min_cluster = inRange_Clustering(candidate_list)
+                    #print ("function return", cluster_list, min_cluster)
+                    print("cluster list for", d, ":" , cluster_list)
+                    print ("minimum cluster", min_cluster)  
+
+                    if cluster_list == {}:
+                        new_candidate_list = fogNeighbour_Clustering(d, candidate_list)
+                        cluster_list, min_cluster = inRange_Clustering(new_candidate_list)
+                        print("cluster list for", d, ":" , cluster_list)
+                        print(node_attrs)
+                        print ("new minimum cluster", min_cluster)  
+
+
             if response_times != {}:
                 minimum_RT = min(response_times, key = response_times.get)
                 time = response_times[minimum_RT]
                 print("Minimum response time for",d, ":", minimum_RT, time)
-            
+     
 
 
-                      
+
+
+ 
+
 print("done")                    
-# for n in G.edges:
-#     if "d" in n[1]:
-#       for keys, values in node_attrs.items():
-#           if n[0] in keys:
-#               print("STR: ",values["STR"])
-        
-#           if n[1] in keys:
-#               print("SIZE:", values["Size"])
-           
-
-"""
-fog_shit = (list(G.adj['d1']))
-print(G.nodes[fog_shit[0]])
-#print(nx.node_connected_component(G, "C"))
-
-for n in G.nodes:
-print(G.adj["d1"])
-"""
-
-#print(node_attrs["f1"])
-#print(device_fog_connections)
-#print(G.nodes["C"])
-
-       
-#print(nx.get_node_attributes(G,"Size"))
-#print(nx.get_node_attributes(G,"STR"))
-#print(link_attrs)
-#print(nx.edges(G))
-#print("list of nodes: ",list(G.nodes))
-#print("list of links: ",list(G.edges))
-#print("Connected nodes: ",list(nx.connected_components(G)))
-#print("sorted Degress: ",sorted(d for n, d in G.degree()))
-#print("clusters: ",nx.clustering(G))
-#print("shortest path from node 3 to connected nodes: ", sp[3])
-
 
 
 nx.draw(G, pos = node_positions, nodelist = ["C"],
